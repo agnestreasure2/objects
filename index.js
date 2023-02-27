e35bfc1cde93c9bcc45bab093f452b6d67edd5f1
Agnes <agnesagbo6@gmail.com>
2023-02-27 04:55:28 -0800

    obe=ject assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+// QUESTION 1:
+function instagramPost(HandleOfAuthour, Content, link, numberOfViews, numberofLikes){
+    this.HandleOfAuthour = HandleOfAuthour;
+    this.Content = Content;
+    this.link = link;
+    this.numberOfViews = numberOfViews;
+    this.numberofLikes = numberofLikes;
+
+}
+
+//QUESTION 2:
+let firstIgPost = new instagramPost('@agneytreasure', 'Overtly ')
